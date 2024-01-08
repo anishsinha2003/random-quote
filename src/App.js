50db9098afaa642294ef4b21b74ddddff6f51cf1
Anish Sinha <anishsinha@Anishs-MBP.modem>
2024-01-08 19:10:50 +1100

    second done

--- src/App.js
+++ src/App.js
@@ -1,64 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import * as marked from 'marked';
 import './App.css';
-import React from "react";
 
-function App() {
-  const quotes = [
-    {
-      quote: "quote 1",
-      author: "author 1"
-    },
-    {
-      quote: "quote 2",
-      author: "author 2"
-    },
-    {
-      quote: "quote 3",
-      author: "author 3"
-    },
-    {
-      quote: "quote 4",
-      author: "author 4"
-    },
-    {
-      quote: "quote 5",
-      author: "author 5"
-    },
-    {
-      quote: "quote 6",
-      author: "author 6"
-    },
-  ]
-  // const [quoteIdx, setQuoteIdx] = React.useState(3)
+const App = () => {
+  const [preview, setPreview] = useState("")
+  const [markdown, setMarkdown] = useState("");
 
-  const [quote, setQuote] = React.useState(quotes[3].quote)
-  const [author, setAuthor] = React.useState(quotes[3].author)
-  function handleClick() {
-    const randomIDX = Math.floor(Math.random() * 6);
-    setQuote(quotes[randomIDX].quote)
-    setAuthor(quotes[randomIDX].author)
-  }
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  useEffect(() => {
+    const html = marked.parse(markdown, { breaks: true });
+    setPreview(html);
+  }, [markdown]);
+  const ReactMarkdown = require('react-markdown')
   return (
-    <div className="App">
-      <div id="quote-box">
-        <div id="text">
-          {quote}
-        </div>
-        <br/>
-        <div id="author">
-          {author}
-        </div>
-        <br/>
-        <button onClick={handleClick} id="new-quote">
-          new quote
-        </button>
-        <br/><br/>
-        <a href="https://twitter.com/intent/tweet" rel="noreferrer" id="tweet-quote" target="_blank" class="tweet-button">
-          Tweet Quote
-        </a>
-
+    <div className="app">
+      <div className="editor-container">
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={handleChange}
+          placeholder="Enter your Markdown here..."
+        />
+      </div>
+      <div
+        id="preview"
+        className="preview-container"
+      >
+        <ReactMarkdown source={markdown}/>
       </div>
     </div>
   );
-}
+};
 
 export default App;
