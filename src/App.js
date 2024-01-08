2e0c7ec9813695003dbd9cf4c3fd6005fe92b4f4
Anish Sinha <anishsinha@Anishs-MBP.modem>
2024-01-08 19:42:31 +1100

    sound

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-import React, { useState } from 'react';
-import Markdown from './Markdown';
-
+import React, { useEffect } from 'react';
+import "./App.css";
 const App = () => {
-  const [markdownText, setMarkdownText] = useState('# Hello, Markdown!');
 
-  const handleInputChange = (e) => {
-    setMarkdownText(e.target.value);
+  const handleKeyPress = (event) => {
+    const key = event.key.toUpperCase();
+    const drumPad = document.getElementById(key);
+    if (drumPad) {
+      drumPad.click();
+    }
   };
 
+  useEffect(() => {
+    document.addEventListener('keydown', handleKeyPress);
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  const clickHandle = () => {
+    const audioElement = document.getElementById("Q");
+    if (audioElement) {
+      audioElement.play();
+    }
+    };
   return (
-    <div>
-      <h1>Markdown Interpreter</h1>
-      <div className="editor-container">
-      <textarea
-        id="editor"
-        value={markdownText}
-        onChange={handleInputChange}
-        rows={10}
-        cols={50}
-      />
+
+    <div id="drum-machine" className="App">
+      <div id="display" className="display">
+        {/* {displayText} */}
       </div>
-      <div
-        id="preview"
-        className="preview-container"
-      >
-      <Markdown markdownText={markdownText} />
+      <div className="drum-pad" onClick={clickHandle}>
+        Q
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        W
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        E
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        S
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        D
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        Z
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+      <div className="drum-pad" onClick={clickHandle}>
+        X
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+      </div>
+      &nbsp;
+
+      <div className="drum-pad" onClick={clickHandle}>
+        C
+        <audio className="clip" id="Q" src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
       </div>
     </div>
   );
